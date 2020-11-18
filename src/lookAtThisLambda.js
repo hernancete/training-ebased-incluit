07a3884285f605cafe3112d3d3d29f8da17afadd
mab777 <martin.a.balmaceda@gmail.com>
2020-11-18 19:10:15 -0300

    subida

--- src/lookAtThisLambda.js
+++ src/lookAtThisLambda.js
@@ -1,14 +1,26 @@
 // Este es el helper que se encarga de parsear los datos segun el inputMode y llamar al domain
 const { commandMapper } = require('ebased/handler');
 
+//agrego la linea del sqs
+const config = require('ebased/util/config');
+const sqs = require('ebased/service/downstream/sqs');
+const CREATE_DEPOSIT_QUEUE = config.get('QUEUE_URL');
+
 // Nuestro input viene de API Gateway -> importamos input/commandApi
 const inputMode = require('ebased/handler/input/commandApi');
 
 // Nuestro lambda responde a API Gateway -> importamos output/commandApi
 const outputMode = require('ebased/handler/output/commandApi');
 
-const domain = async (commandPayload, commandMeta) => {
-  return { body: 'pepe' };
+const domain = async ({commandPayload, commandMeta}) => {
+  
+  const sqsSendParams = {
+    QueueUrl: CREATE_DEPOSIT_QUEUE,
+    MessageBody: "prueba",
+  };
+  await sqs.send(sqsSendParams, commandMeta);
+  
+  return { body: 'Repuesta '+ commandPayload };
 };
 
 module.exports.handler = async (command, context) => {
